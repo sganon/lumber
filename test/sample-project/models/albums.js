b1bf712606abf2c63ad3917b080735e1aeabb479
Sandro Munda <sandro@munda.me>
2018-06-20 12:24:06 +0200

    WIP

--- test/sample-project/models/albums.js
+++ test/sample-project/models/albums.js
@@ -2,10 +2,6 @@
 
 module.exports = (sequelize, DataTypes) => {
   var Model = sequelize.define('albums', {
-    'AlbumId': {
-      type: DataTypes.INTEGER,
-      primaryKey: true 
-    },
     'Title': {
       type: DataTypes.STRING,
     },
